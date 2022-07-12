7fec85c110b14d8f0e130ffbaad57747d7b0d5d5
Leonardo Viana <leonardoviana00@hotmail.com>
2022-07-12 08:49:34 -0300

    feature(k8s-shell-vagrant): add script start

---
+++ scripts/bootstrap_k8s_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "[TASK 01] - STATUS DOS SERVIÇOS"
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+echo "DOCKER"
+systemctl status docker | grep "Active:"
+echo "K8S"
+systemctl status kubelet | grep "Active:"
+
+echo "[TASK 02] - INFORMAÇÕES DO CLUSTER"
+kubectl cluster-info
+
+echo "[TASK 03] - LISTAR TUDO"
+kubectl get all --all-namespaces
+
+echo "[TASK 04] - LISTAR NODES K8S..."
+kubectl get nodes
+
+echo "[TASK 05] - LISTAR TODOS OS NAMESPACES PADRÃO"
+kubectl get ns
